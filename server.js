dfdbfe0b5746d95efeb84a4739249bb6303c982c
charan <charanr@agilitegroup.com>
2024-09-15 08:27:16 -0400

    updated cors

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
-
+const cors = require('cors');
 dotenv.config();
 const app = express();
-
+app.use(cors())
 app.use(express.json());
 
-app.use('/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
