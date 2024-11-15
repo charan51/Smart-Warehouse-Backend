53014c4d291651fff7a8c24c628e98df2a72a943
Karthik S G <sg.karthik18@gmail.com>
2024-11-15 15:30:16 -0500

    Added lowstock API

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const {
   getDashboardList,
-  getSupplierList
+  getSupplierList,
+  getLowStockList
 } = require("../controllers/dashboardController");
 const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
-
+router.get("/getLowStock", getLowStockList);
 router.get("/getWarehouseDashboard", getDashboardList);
 router.get("/getSupplierDashboard", getSupplierList);
 router.get("/getInventoryDashboard", getSupplierList);
